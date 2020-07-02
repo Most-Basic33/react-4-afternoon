964f51ca72c064b6d05d5f6f20d5b5376258171e
bishopwalker <walkerbishop333@gmail.com>
2020-07-02 12:03:51 -0700

    step 6

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      students: []
+    }
+  }
+//utilize the match method from react-router-dom object
+  componentDidMount() {
+axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+.then(results=>{
+  this.setState({
+    students: results.data
+  })
+})
+.catch(err=>console.log(err))
   }
 
   render() {
+    const mappedStudents = this.state.students.map((student,i) =>{
+      return(
+        <div key={i} >
+      <h3>{student.first_name} {student.last_name } </h3>
+        </div>
+      )
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
+        { mappedStudents }
 
       </div>
     )
